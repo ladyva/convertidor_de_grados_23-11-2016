5785cff9b3169e44c0c4e52e729e5a2ee885ce14
usuario <usuario@usuario-HP-Compaq-6000-Pro-MT-PC>
2016-11-21 19:50:49 -0300

    MI PRIMER IMPORTE A GIT HUB

---
+++ HOLA_GIT/src/hola_git/HOLA_GIT.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hola_git;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author usuario
+ */
+public class HOLA_GIT {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String n;
+        n= JOptionPane.showInputDialog("Ingrese su nombre");
+        JOptionPane.showMessageDialog(null, "Hola  "+n+"\nsBIEMBENIDO A GIT HUB");
+    }
+    
+}
